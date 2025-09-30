1d7bfcb9d7f8b6c0c2935e754753d10d181c5214
mygoazure-KB <mygoazure@gmail.com>
2025-09-30 16:20:38 +0530

    Added new content in index.cshtml

--- obj/Release/net8.0/CoreWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/CoreWebApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreWebApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a8632844ac4af8875a4130abb33ad1480cdb47")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreWebApp")]
