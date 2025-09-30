5be1396afe68840a98de9b18430b54d265ad3dfb
mygoazure-KB <mygoazure@gmail.com>
2025-09-30 16:09:49 +0530

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
